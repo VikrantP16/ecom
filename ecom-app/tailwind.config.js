623a046bf6043da7d5e3c743531c51588c221975
VikrantP16 <vikrantpatel1608@gmail.com>
2024-12-23 18:41:23 +0530

    update Navbar.jsx

--- ecom-app/tailwind.config.js
+++ ecom-app/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        tiny: '8px',
+      }
+    },
   },
   plugins: [],
 }
